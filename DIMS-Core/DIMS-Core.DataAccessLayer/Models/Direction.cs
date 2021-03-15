c5fe1fe9755ab734fce544a87b6260ce2060a349
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-03-15 20:19:34 +0300

    Models scaffolding

--- DIMS-Core/DIMS-Core.DataAccessLayer/Models/Direction.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Models/Direction.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
+#nullable disable
+
 namespace DIMS_Core.DataAccessLayer.Models
 {
     public partial class Direction
     {
         public Direction()
         {
-            UserProfile = new HashSet<UserProfile>();
+            UserProfiles = new HashSet<UserProfile>();
         }
 
         public int DirectionId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public virtual ICollection<UserProfile> UserProfile { get; set; }
+        public virtual ICollection<UserProfile> UserProfiles { get; set; }
     }
 }
